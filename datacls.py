6dfbae6b6e51ddb399c4d21ddd68a40a4b9f8482
Tom Ritchford <tom@swirly.com>
2022-06-06 14:56:36 +0200

    Version 4.1.0

--- datacls.py
+++ datacls.py
@@ -23,7 +23,7 @@ __all__ = (
     'mutable',
     'replace',
 )
-__version__ = '4.0.0'
+__version__ = '4.1.0'
 
 _METHODS = 'asdict', 'astuple', 'fields', 'replace'
 _CLASS_METHODS = {'field_names', 'fields'}
